956c99816616648f0da66b2e39593a46097c6044
Alex <49230683+Stea11er@users.noreply.github.com>
2019-11-20 21:28:41 +0300

    3

--- SimpLI/mainwindow.cpp
+++ SimpLI/mainwindow.cpp
@@ -22,10 +22,9 @@ void MainWindow::on_pushButton_clicked()
     bool ok;
     double a = str_x.toDouble(&ok);     //Начало интервала, где рисуем график по оси Ox
     double b = str_y.toDouble(&ok);     //Конец интервала, где рисуем график по оси Ox
-    QCPItemTracer *tracer = new QCPItemTracer(ui->widget);
 
     if (ui->radioButton->isChecked()) {
-    //Рисуем график y=x*x
+    ui->widget->clearGraphs();
 
     //Начало интервала, где рисуем график по оси Ox
     //Конец интервала, где рисуем график по оси Ox
@@ -54,130 +53,120 @@ void MainWindow::on_pushButton_clicked()
     ui->widget->yAxis->setLabel("y");
 
     //Установим область, которая будет показываться на графике
-    ui->widget->xAxis->setRange(a, b);//Для оси Ox
+    ui->widget->xAxis->setRange((a-20), (b+20));
 
-    //Для показа границ по оси Oy сложнее, так как надо по правильному
-    //вычислить минимальное и максимальное значение в векторах
     double minY = y[0], maxY = y[0];
     for (int i=1; i<N; i++)
     {
         if (y[i]<minY) minY = y[i];
         if (y[i]>maxY) maxY = y[i];
     }
-    ui->widget->yAxis->setRange(minY, maxY);//Для оси Oy
+    ui->widget->yAxis->setRange((minY-20), (maxY+20));//Для оси Oy
     } else if (ui->radioButton_2->isChecked()) {
-            //Рисуем точки
+            int N=5;
+            QVector<double> x(N), y(N);
 
-            int N=5; //Допустим, что у нас пять точек
-            QVector<double> x(N), y(N); //Массивы координат точек
-
-            //Зададим наши точки
             x[0] = 1.0; y[0] = 2.0;
             x[1] = 4.0; y[1] = 1.0;
             x[2] = 3.0; y[2] = 0.0;
             x[3] = 0.5; y[3] = 2.2;
             x[4] = 1.5; y[4] = 0.7;
 
-            ui->widget->clearGraphs();//Если нужно, но очищаем все графики
-            //Добавляем один график в widget
+            ui->widget->clearGraphs();
             ui->widget->addGraph();
-            //Говорим, что отрисовать нужно график по нашим двум массивам x и y
             ui->widget->graph(0)->setData(x, y);
 
-            ui->widget->graph(0)->setPen(QColor(50, 50, 50, 255));//задаем цвет точки
-            ui->widget->graph(0)->setLineStyle(QCPGraph::lsNone);//убираем линии
-            //формируем вид точек
+            ui->widget->graph(0)->setPen(QColor(255, 0, 0, 255));
+            ui->widget->graph(0)->setLineStyle(QCPGraph::lsNone);
             ui->widget->graph(0)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 4));
 
-            //Подписываем оси Ox и Oy
             ui->widget->xAxis->setLabel("x");
             ui->widget->yAxis->setLabel("y");
 
-            //Установим область, которая будет показываться на графике
-            ui->widget->xAxis->setRange(-1, 5);//Для оси Ox
-            ui->widget->yAxis->setRange(-1, 3);//Для оси Oy
+            ui->widget->xAxis->setRange(-1, 5);
+            ui->widget->yAxis->setRange(-1, 3);
     } else if (ui->radioButton_3->isChecked()) {
-        // Инициализируем вертикальную линию
-        //verticalLine = new QCPCurve(ui->widget->xAxis, ui->widget->yAxis);
+        // set dark background gradient:
+        QLinearGradient gradient(0, 0, 0, 400);
+        gradient.setColorAt(0, QColor(90, 90, 90));
+        gradient.setColorAt(0.38, QColor(105, 105, 105));
+        gradient.setColorAt(1, QColor(70, 70, 70));
+        ui->widget->setBackground(QBrush(gradient));
 
-        // Подключаем сигналы событий мыши от полотна графика к слотам для их обработки
-        connect(ui->widget, &QCustomPlot::mousePress, this, &MainWindow::slotMousePress);
-        connect(ui->widget, &QCustomPlot::mouseMove, this, &MainWindow::slotMouseMove);
+        // create empty bar chart objects:
+        QCPBars *regen = new QCPBars(ui->widget->xAxis, ui->widget->yAxis);
+        QCPBars *nuclear = new QCPBars(ui->widget->xAxis, ui->widget->yAxis);
+        QCPBars *fossil = new QCPBars(ui->widget->xAxis, ui->widget->yAxis);
+        regen->setAntialiased(false); // gives more crisp, pixel aligned bar borders
+        nuclear->setAntialiased(false);
+        fossil->setAntialiased(false);
+        regen->setStackingGap(1);
+        nuclear->setStackingGap(1);
+        fossil->setStackingGap(1);
+        // set names and colors:
+        fossil->setName("Fossil fuels");
+        fossil->setPen(QPen(QColor(111, 9, 176).lighter(170)));
+        fossil->setBrush(QColor(111, 9, 176));
+        nuclear->setName("Nuclear");
+        nuclear->setPen(QPen(QColor(250, 170, 20).lighter(150)));
+        nuclear->setBrush(QColor(250, 170, 20));
+        regen->setName("Regenerative");
+        regen->setPen(QPen(QColor(0, 168, 140).lighter(130)));
+        regen->setBrush(QColor(0, 168, 140));
+        // stack bars on top of each other:
+        nuclear->moveAbove(fossil);
+        regen->moveAbove(nuclear);
 
-        // создаём вектора для вертикальной линии
-        //QVector<double> x(2) , y(2);
-       //     x[0] = 0;
-         //   y[0] = -50;
-        //    x[1] = 0;
-         //   y[1] = 50;
+        // prepare x axis with country labels:
+        QVector<double> ticks;
+        QVector<QString> labels;
+        ticks << 1 << 2 << 3 << 4 << 5 << 6 << 7;
+        labels << "USA" << "Japan" << "Germany" << "France" << "UK" << "Italy" << "Canada";
+        QSharedPointer<QCPAxisTickerText> textTicker(new QCPAxisTickerText);
+        textTicker->addTicks(ticks, labels);
+        ui->widget->xAxis->setTicker(textTicker);
+        ui->widget->xAxis->setTickLabelRotation(60);
+        ui->widget->xAxis->setSubTicks(false);
+        ui->widget->xAxis->setTickLength(0, 4);
+        ui->widget->xAxis->setRange(0, 8);
+        ui->widget->xAxis->setBasePen(QPen(Qt::white));
+        ui->widget->xAxis->setTickPen(QPen(Qt::white));
+        ui->widget->xAxis->grid()->setVisible(true);
+        ui->widget->xAxis->grid()->setPen(QPen(QColor(130, 130, 130), 0, Qt::DotLine));
+        ui->widget->xAxis->setTickLabelColor(Qt::white);
+        ui->widget->xAxis->setLabelColor(Qt::white);
 
-        //ui->widget->plottable(verticalLine);    // Добавляем линию на полотно
-        //ui->widget->addLayer(verticalLine)
-        //verticalLine->setName("Vertical");      // Устанавливаем ей наименование
-        //verticalLine->setData(x, y);            // И устанавливаем координаты
+        // prepare y axis:
+        ui->widget->yAxis->setRange(0, 12.1);
+        ui->widget->yAxis->setPadding(5); // a bit more space to the left border
+        ui->widget->yAxis->setLabel("Power Consumption in\nKilowatts per Capita (2007)");
+        ui->widget->yAxis->setBasePen(QPen(Qt::white));
+        ui->widget->yAxis->setTickPen(QPen(Qt::white));
+        ui->widget->yAxis->setSubTickPen(QPen(Qt::white));
+        ui->widget->yAxis->grid()->setSubGridVisible(true);
+        ui->widget->yAxis->setTickLabelColor(Qt::white);
+        ui->widget->yAxis->setLabelColor(Qt::white);
+        ui->widget->yAxis->grid()->setPen(QPen(QColor(130, 130, 130), 0, Qt::SolidLine));
+        ui->widget->yAxis->grid()->setSubGridPen(QPen(QColor(130, 130, 130), 0, Qt::DotLine));
 
-        // создаём вектора для графика
-        QVector<double> x1(5) , y1(5);
-            x1[0] = -45;
-            y1[0] = -43;
-            x1[1] = 46;
-            y1[1] = 42;
-            x1[2] = -25;
-            y1[2] = -24;
-            x1[3] = -12;
-            y1[3] = 10;
-            x1[4] = 25;
-            y1[4] = 26;
+        // Add data:
+        QVector<double> fossilData, nuclearData, regenData;
+        fossilData  << 0.86*10.5 << 0.83*5.5 << 0.84*5.5 << 0.52*5.8 << 0.89*5.2 << 0.90*4.2 << 0.67*11.2;
+        nuclearData << 0.08*10.5 << 0.12*5.5 << 0.12*5.5 << 0.40*5.8 << 0.09*5.2 << 0.00*4.2 << 0.07*11.2;
+        regenData   << 0.06*10.5 << 0.05*5.5 << 0.04*5.5 << 0.06*5.8 << 0.02*5.2 << 0.07*4.2 << 0.25*11.2;
+        fossil->setData(ticks, fossilData);
+        nuclear->setData(ticks, nuclearData);
+        regen->setData(ticks, regenData);
 
-        // Добавляем график на полотно
-        ui->widget->addGraph(ui->widget->xAxis, ui->widget->yAxis);
-        ui->widget->graph(0)->setData(x1,y1);     // Устанавливаем координаты точек графика
-
-        // Инициализируем трассировщик
-        //QCPItemTracer *tracer = new QCPItemTracer(ui->widget);
-        tracer->setGraph(ui->widget->graph(0));   // Трассировщик будет работать с графиком
-
-        // Подписываем оси координат
-        ui->widget->xAxis->setLabel("x");
-        ui->widget->yAxis->setLabel("y");
-
-        // Устанавливаем максимальные и минимальные значения координат
-        ui->widget->xAxis->setRange(-50,50);
-        ui->widget->yAxis->setRange(-50,50);
+        // setup legend:
+        ui->widget->legend->setVisible(true);
+        ui->widget->axisRect()->insetLayout()->setInsetAlignment(0, Qt::AlignTop|Qt::AlignHCenter);
+        ui->widget->legend->setBrush(QColor(255, 255, 255, 100));
+        ui->widget->legend->setBorderPen(Qt::NoPen);
+        QFont legendFont = font();
+        legendFont.setPointSize(10);
+        ui->widget->legend->setFont(legendFont);
+        ui->widget->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
     }
-    //И перерисуем график на нашем widget
     ui->widget->replot();
-}
-void MainWindow::slotMousePress(QMouseEvent *event)
-{
-    if (ui->radioButton_3->isChecked()) {
-    QCPItemTracer *tracer = new QCPItemTracer(ui->widget);
-    // Определяем координату X на графике, где был произведён клик мышью
-    double coordX = ui->widget->xAxis->pixelToCoord(event->pos().x());
-
-        // Подготавливаем координаты по оси X для переноса вертикальной линии
-        //QVector<double> x(2), y(2);
-        //x[0] = coordX;
-        //y[0] = -50;
-        //x[1] = coordX;
-        //y[1] = 50;
-
-        // Устанавливаем новые координаты
-        //verticalLine->setData(x, y);
-
-        // По координате X клика мыши определим ближайшие координаты для трассировщика
-        tracer->setGraphKey(coordX);
-
-        // Выводим координаты точки графика, где установился трассировщик, в lineEdit
-        ui->widget->replot(); // Перерисовываем содержимое полотна графика
-    }
-}
-
-void MainWindow::slotMouseMove(QMouseEvent *event)
-{
-    /* Если при передвижении мыши, ей кнопки нажаты,
-     * то вызываем отработку координат мыши
-     * через слот клика
-     * */
-    if(QApplication::mouseButtons()) slotMousePress(event);
 }
